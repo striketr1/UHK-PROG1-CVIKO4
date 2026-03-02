e23827a25fdd1c97dc2e21a0d5eaf288dddf009a
vancust1 <vancura.stanislav@seznam.cz>
2026-03-02 12:53:14 +0100

    Upravení

--- src/main/java/pro1/Tools1.java
+++ src/main/java/pro1/Tools1.java
@@ -3,7 +3,8 @@ package pro1;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Tools1
 {
@@ -12,10 +13,14 @@ public class Tools1
         List<String> lines;
         try {
             lines = Files.readAllLines(path);
+            HashMap<String,Integer> dict = new HashMap<>();
+            for(var line:  lines){
+               var current = dict.getOrDefault(line,0);
+                dict.put(line,current+1);
+            }
+            return dict.keySet().stream().sorted(Comparator.comparing(o -> o)).map((x)->x+":"+dict.get(x)).collect(Collectors.joining(","));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        return "Jasno:0,Zataženo:0";
     }
 }
--- src/main/java/pro1/Tools4.java
+++ src/main/java/pro1/Tools4.java
@@ -1,10 +1,22 @@
 package pro1;
 
+import java.util.Arrays;
+
 public class Tools4
 {
     public static double maximumValue(Number[] numbers)
     {
         double max = 0;
+        for(var n :  numbers){
+            max = Math.max(n.doubleValue(), max);
+            //NEBO
+            //if( n.doubleValue() > max ){
+            //    max = n.doubleValue();
+            //}
+            }
+        //NEBO
+        //return Arrays.stream(numbers).mapToDouble(Number::doubleValue).max().getAsDouble();
+
         return max;
     }
 }
